110c70c3cfc75e445128d93867f374830c463a51
Mariano Scazzariello <marianoscazzariello@gmail.com>
2026-04-02 18:20:10 +0200

    Minor fixes

--- start_prod.sh
+++ start_prod.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -e
+
 sudo -E PATH=$PATH SUDO_UID=0 bash -c "cd ../ixp-digital-twin/ && .venv/bin/python3 deploy_external_link.py"
 docker compose -f docker-compose.yml up -d --build
